d3ce41fbae8dc4c0f901fbb60bf6d753cb5cfa60
brunolepis <brunka215@gmail.com>
2024-09-23 14:56:16 +0200

    chore: clearer function parameter name

--- src/index.ts
+++ src/index.ts
@@ -5,33 +5,35 @@ export { Options, Term };
 
 export async function getTerm(query: string, options?: Options): Promise<Term>;
 export async function getTerm(
-  str: string[],
+  query: string[],
   options?: Options
 ): Promise<Term[]>;
 /**
  * Get a term with its description and the example from the urban dictionary
- * @param {string} str - The term name
- * @param {boolean} formatMarkdown - Whether to format the term description and the example for markdown
+ * @param {string} query - The term name
+ * @param {boolean} options.formatMarkdown - Whether to format the term description and the example for markdown
  * @example await getTerm("urban");
  */
 export async function getTerm(
-  str: string | string[],
+  query: string | string[],
   options: Options = { formatMarkdown: false }
 ): Promise<Term | Term[]> {
-  if (!str || str.length === 0) throw new Error("Provide a term to search for");
+  if (!query || query.length === 0)
+    throw new Error("Provide a term to search for");
+
   if (typeof options.formatMarkdown !== "boolean")
     throw new Error("Format markdown option needs to be a boolean");
 
-  if (Array.isArray(str)) {
+  if (Array.isArray(query)) {
     const responses: Term[] = [];
-    for (const query of str) {
-      const termData = await get(query);
+    for (const searchQuery of query) {
+      const termData = await get(searchQuery);
       responses.push(format(termData, options.formatMarkdown));
     }
     return responses;
   }
 
-  const termData = await get(str);
+  const termData = await get(query);
 
   return format(termData, options.formatMarkdown);
 }
